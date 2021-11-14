d09fe0b842bbb6fad00158ee83fba0a036ff8498
abdullahnazzal <a.nazzal1995@gmail.com>
2021-11-14 14:30:09 +0200

    first commit

---
+++ snacks/tests.py
@@ -0,0 +1,16 @@
+from django import urls
+from django.http import response
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+# Create your tests here.
+class snacksTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+    
+    def test_about_us_page_template(self):
+        url = reverse('about-us')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response,'about_us.html')
